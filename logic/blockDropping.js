507769730da212ed6880a8c7c973b655a5f1509c
Hugo <hugolopezvalladares21@gmail.com>
2026-04-22 22:00:56 +0300

    Deleted debug lines and test function

--- logic/blockDropping.js
+++ logic/blockDropping.js
@@ -1,29 +1,4 @@
 function gravityDrop(board, speed){
-    board[3][0] = 2;
-    board[8][0] = null;
-    console.table(board)
-    for(let y = rows-1; y>= 0; y--){
-        for (let x = 0; x< columns;x++){
-            if (board[y][x] === null){
-                for(let j = y; j> 0; j--){
-                    if (board[j][x] === null){
-                        if(board[j-1][x] != null){
-                            board[j][x] = board[j-1][x];
-                            board[j-1][x] = null;
-                            j+=2;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return board
-}
-
-function gravityDrop(board, speed){
-    board[3][0] = 2;
-    board[8][0] = null;
-    console.table(board)
     let moved = true;
     while(moved){
         moved = false;
